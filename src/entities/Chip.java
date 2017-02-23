2c00abdca635205d068712035125e9b4dc0c8c94
shroman3 <roma1988@gmail.com>
2017-02-23 12:42:41 +0200

    Update Chip.java

--- src/entities/Chip.java
+++ src/entities/Chip.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * SSDPlayer Visualization Platform (Version 1.0)
  * Authors: Roman Shor, Gala Yadgar, Eitan Yaakobi, Assaf Schuster
- * Copyright (c) 2015, Technion – Israel Institute of Technology
+ * Copyright (c) 2015, Technion â€“ Israel Institute of Technology
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that
  * the following conditions are met:
@@ -180,7 +180,7 @@ public abstract class Chip<P extends Page, B extends Block<P>, T extends Plane<P
 	}
 
 	public int getTotalGCInvocations() {
-		return this.totalGCInvocations;
+		return totalGCInvocations;
 	}
 
 	public EntityInfo getInfo() {
